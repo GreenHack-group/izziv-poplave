dbe0a3881536a7df4911650a9c87743a0505e2e8
ltomazic <62839996+ltomazic@users.noreply.github.com>
2021-11-04 22:11:49 +0100

    Add connectionDB

---
+++ backend/pozivnikAPI/pozivnik.Persistence/Implementation/ConnectionDB.cs
@@ -0,0 +1,17 @@
+﻿using pozivnik.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pozivnik.Persistence.Implementation
+{
+    public class ConnectionDB : IConnectionDB
+    {
+        public string getDB()
+        {
+            return "";
+        }
+    }
+}
