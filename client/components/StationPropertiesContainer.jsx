ad9d630cf66b53e1cbf67981320e0b1a30b22484
jknfic <jk2922@student.uni-lj.si>
2021-11-07 00:06:50 +0100

    Manjkajo še animacije in grafi

--- client/components/StationPropertiesContainer.jsx
+++ client/components/StationPropertiesContainer.jsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
         //paddingVertical: theme.LAYOUT.paddingLarge,
         //paddingBottom: theme.LAYOUT.paddingLarge,
         //paddingHorizontal: theme.LAYOUT.paddingLarge,
-        backgroundColor: theme.COLORS.softBlue,
+        backgroundColor: theme.COLORS.background,
         color: theme.COLORS.black,
         flex: 1,
     },
