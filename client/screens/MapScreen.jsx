06962ff3e6846e96cbdc15fbc4214414e2985124
MartinStrekelj <martin.strekelj123@gmail.com>
2021-11-06 20:54:30 +0100

    Client: Fix merge errors

--- client/screens/MapScreen.jsx
+++ client/screens/MapScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Dimensions } from 'react-native'
 import { fetchStations } from '../Api/BackendAPI'
@@ -6,6 +6,7 @@ import { Container } from '../components/Container'
 import { Map } from '../components/Map'
 import { BottomDweller } from '../components/BottomDweller'
 import { ListItem } from '../components/StationList/ListItem'
+import { StationsContext } from '../context/StationsContext'
 
 /**
  * Screen with a map widget
@@ -13,30 +14,19 @@ import { ListItem } from '../components/StationList/ListItem'
  * @returns
  */
 export const MapScreen = (props) => {
-    const [stations, setStations] = useState([])
-    const [isLoading, setLoading] = useState(false)
+    const { stations, isLoading } = useContext(StationsContext)
     const [selectedStation, setSelectedStation] = useState(null)
     const mapRef = useRef(null)
     const dwellerRef = useRef(null)
 
     useEffect(() => {
-        retrieveStationsFromAPI()
         if (dwellerRef.current) dwellerRef.current.close()
     }, [])
-
-    const retrieveStationsFromAPI = async () => {
-        setLoading(true)
-        console.log('Stations fetching ...')
-        const data = await fetchStations()
-        setLoading(false)
-        setStations(data)
-    }
 
     const dimensions = Dimensions.get('window')
 
     const handleInfoPress = (stationId) => {
         props.navigation.navigate('Station', { stationId })
-        console.log('clicked on station info')
     }
 
     const animateToStation = (station) => {
@@ -53,11 +43,9 @@ export const MapScreen = (props) => {
     }
 
     const handleOnMarkerPress = (station) => {
-        console.log(station)
         setSelectedStation(station)
         animateToStation(station)
         if (dwellerRef.current) dwellerRef.current.expand()
-        console.log('pritisnjen station na mapi')
     }
 
     return (
